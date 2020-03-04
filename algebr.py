6a14dfd678a050ce4aeb156112abaabbf767ad4e
IDK-png <59571985+IDK-png@users.noreply.github.com>
2020-03-04 18:09:19 +0200

    Ты пчела я пчеловод хуй пизда зубной налет

--- algebr.py
+++ algebr.py
@@ -8,34 +8,21 @@ class First:
             except ValueError:
                 if ord(a.upper()) != 32:
                     tonext.append(a)
-      for b,a in enumerate(tonext): #  If the letter before equal
-          try:
-                  print(a)
-                  if type(a) == int and type(tonext[b+2]) == str and len(letter) < 2 and tonext.index("=")>tonext.index(a) and tonext[b+2].isalpha():
-                      letter.append(tonext[tonext.index(a)+1]);letter.append(a);letter.append(0);tonext.pop(tonext.index(a));tonext.pop(tonext.index(tonext[b+2]))
-                  if type(a) == int and type(tonext[b+2]) == str and len(letter) < 2 and tonext.index("=")<tonext.index(a) and tonext[b+2].isalpha():
+      for a in tonext: 
+                  if type(a) == str and tonext.index("=")>tonext.index(a) and a.isalpha(): #  If the letter before equal
                       letter.append(a);letter.append(0);tonext.pop(tonext.index(a))
                   elif type(a) == str and tonext.index("=")<tonext.index(a) and a.isalpha(): # If the letter after equal
                       letter.append(a);letter.append(1);tonext.pop(tonext.index(a))
                   print(letter)
-          except IndexError:
-              print("")
-      for b,a in enumerate(tonext):
-        try:
+      for a in tonext:
           if "+" in tonext:
               what.append("+")
-              if type(a) == int and type(tonext[b+2]) == int:
-                  nums.append(int(str(a) + str(tonext[b+2])))
-              elif type(a) == int and type(tonext[b+2]) == str:
+              if type(a) == int:
                   nums.append(int(a))
           elif "-" in tonext:
               what.append("-")
-              if type(a) == int and type(tonext[b+2]) == int:
-                  nums.append(int(str(a) + str(tonext[b+2])))
-              elif type(a) == int and type(tonext[b+2]) == str:
+              if type(a) == int:
                   nums.append(int(a))
-        except IndexError:
-              print("")
       if 1 in letter:
           if "+" in what:
             print(f"{letter[0]} = {sum(nums)}")
